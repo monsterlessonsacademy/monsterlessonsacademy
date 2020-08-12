47825b8e51c92c38dae3c671d5dac74f1284457a
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-08-12 23:01:16 +0200

    How angular starts application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-for-beginners';
-}
+export class AppComponent {}
