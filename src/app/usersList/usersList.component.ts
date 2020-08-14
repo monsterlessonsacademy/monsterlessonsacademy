873d3748138a8b73054920ee89ecba3ec845aa13
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-08-14 21:47:52 +0200

    Rendering data in angular template

--- src/app/usersList/usersList.component.ts
+++ src/app/usersList/usersList.component.ts
@@ -4,4 +4,24 @@ import {Component} from '@angular/core'
   selector: 'app-users-list',
   templateUrl: './usersList.component.html'
 })
-export class UsersListComponent {}
+export class UsersListComponent {
+  // testUsers = ['Jack', 'John', 'Sam']
+  users = [
+    {
+      id: '1',
+      name: 'Jack',
+      age: 21
+    },
+    {
+      id: '2',
+      name: 'John',
+      age: 25
+    },
+
+    {
+      id: '3',
+      name: 'Sam',
+      age: 29
+    }
+  ]
+}
