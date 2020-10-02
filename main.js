7fe6936ba548a980465e1a603e83a46dab66904f
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-10-02 21:48:25 +0200

    moment js is deprecated

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const first = "first";
-const second = "second";
-const third = "third";
-const forth = "forth";
+const oldDate = luxon.DateTime.local();
+const result = oldDate
+  .setZone("America/New_York")
+  .minus({ weeks: 1 })
+  .startOf("day")
+  .toISO();
+
+console.log("oldDate", oldDate.toString());
+console.log(result);
