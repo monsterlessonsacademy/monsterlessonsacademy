84f26703c0847e2433063bcbb902149af2ce7fab
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-10-04 23:12:44 +0200

    Query string library

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const first = "first";
-const second = "second";
-const third = "third";
-const forth = "forth";
+const queryString = require("query-string");
+
+// const parsed = queryString.parse(location.search);
+// console.log("parsed", parsed);
+//
+// const stringified = queryString.stringify({
+//   sort: "asc",
+//   filter: "title"
+// });
+
+// console.log("stringified", stringified);
+const apiUrl = "https://google.com/api/somearticle?author=foo";
+
+const parsedUrl = queryString.parseUrl(apiUrl);
+const queryParams = {
+  limit: 20,
+  offset: 0,
+  ...parsedUrl.query
+};
+const stringifiedQueryParams = queryString.stringify(queryParams);
+const urlWithParams = `${parsedUrl.url}?${stringifiedQueryParams}`;
+console.log(urlWithParams);
