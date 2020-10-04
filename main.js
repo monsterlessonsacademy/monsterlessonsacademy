f7d49ee9809923671271bed9fba84860b257e7a0
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-10-04 18:41:15 +0200

    Bem css

--- main.js
+++
@@ -1,9 +0,0 @@
-const oldDate = luxon.DateTime.local();
-const result = oldDate
-  .setZone("America/New_York")
-  .minus({ weeks: 1 })
-  .startOf("day")
-  .toISO();
-
-console.log("oldDate", oldDate.toString());
-console.log(result);
