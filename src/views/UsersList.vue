6e3305ce997d1b80ef926a9d6ef592db673e9ffe
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-10-05 15:26:52 +0200

    vue-event-binding-and-property-binding

--- src/views/UsersList.vue
+++ src/views/UsersList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
+    <input type="text" v-model="newUserName" />
+    <button @click="addUser()">Add user</button>
     <div v-for="user in users" :key="user.id" class="user">
       {{ user.name }} <span @click="removeUser(user.id)">Remove</span>
     </div>
+    <a :href="url">Some url</a>
   </div>
 </template>
 
@@ -15,22 +18,38 @@ export default {
         {
           id: "1",
           name: "User 1",
-          age: 20
+          age: 20,
         },
         {
           id: "2",
           name: "User 2",
-          age: 25
+          age: 25,
         },
 
         {
           id: "3",
           name: "User 3",
-          age: 30
-        }
-      ]
+          age: 30,
+        },
+      ],
+      newUserName: "",
+      url: "https://google.com",
     };
-  }
+  },
+  methods: {
+    removeUser(id) {
+      this.users = this.users.filter((user) => user.id !== id);
+    },
+    addUser() {
+      const uniqueId = Math.random().toString(16);
+      const newUser = {
+        id: uniqueId,
+        name: this.newUserName,
+        age: 30,
+      };
+      this.users.push(newUser);
+    },
+  },
 };
 </script>
 
