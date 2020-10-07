41cb60da03e7d05d50525cdb0c325182b9525b8e
Oleksandr Kocherhin <oko@facelift-bbt.com>
2020-10-07 21:19:57 +0200

    34-creating-components-in-vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "@/views/Home.vue";
+import Home from "@/views/Home";
+import UsersList from "@/views/UsersList";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/users",
+    name: "UsersList",
+    component: UsersList
   }
 ];
 
---
+++ src/views/UsersList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-for="user in users" :key="user.id">
+    {{ user.name }} is {{ user.age }} years old
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UsersList",
+  data() {
+    return {
+      users: [
+        {
+          id: "1",
+          name: "User 1",
+          age: 20
+        },
+        {
+          id: "2",
+          name: "User 2",
+          age: 25
+        },
+        {
+          id: "3",
+          name: "User 3",
+          age: 30
+        }
+      ]
+    };
+  }
+};
+</script>
