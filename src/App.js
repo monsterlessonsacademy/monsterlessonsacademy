8444194e8b6b8c906112b68c1eb03555bfbb87cc
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-04 22:05:32 +0100

    55-use-ref-hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useRef, useState } from "react";
 const App = () => {
-  return <h1>Hello react hooks</h1>;
+  const [username, setUsername] = useState("");
+  const textInput = useRef();
+  const fooRef = useRef();
+  console.log("textInput", textInput);
+  console.log("username", username);
+  const onTextFocus = () => {
+    console.log("onTextFocus", textInput.current.value);
+    textInput.current.focus();
+    fooRef.current = "fooo";
+    console.log("ffoRef", fooRef);
+  };
+  return (
+    <div>
+      Hello
+      <input
+        type="text"
+        value={username}
+        ref={textInput}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <div>Foo: {fooRef.current}</div>
+      <button onClick={onTextFocus}>Focus on input pls</button>
+    </div>
+  );
 };
 
 export default App;
