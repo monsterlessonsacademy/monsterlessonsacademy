9d95bde4376f3db4fd51c1ed529da037b50d6421
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-04 21:19:50 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { useContext } from "react";
-import { ThemeContext } from "./index";
-
 const App = () => {
-  const [theme, toggleTheme] = useContext(ThemeContext);
-  console.log("theme", theme);
-  return (
-    <h1 style={{ background: theme.background, color: theme.foreground }}>
-      Hello monsterlessons
-      <button onClick={toggleTheme}>Toggle theme</button>
-    </h1>
-  );
+  return <h1>Hello react hooks</h1>;
 };
 
 export default App;
