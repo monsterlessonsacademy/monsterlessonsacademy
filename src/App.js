8e0438a75f9ca4efcabe8447e63b4cfedb0ef620
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-05 21:51:10 +0100

    56-use-memo-hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import { useRef, useState } from "react";
+import { useState, useMemo } from "react";
+const users = [
+  {
+    id: "1",
+    name: "Foo",
+  },
+  {
+    id: "2",
+    name: "bar",
+  },
+];
 const App = () => {
-  const [username, setUsername] = useState("");
-  const textInput = useRef();
-  const fooRef = useRef();
-  console.log("textInput", textInput);
-  console.log("username", username);
-  const onTextFocus = () => {
-    console.log("onTextFocus", textInput.current.value);
-    textInput.current.focus();
-    fooRef.current = "fooo";
-    console.log("ffoRef", fooRef);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+  console.log("text", text);
+  console.log("search", search);
+  const handleText = (e) => {
+    setText(e.target.value);
   };
+  const handleSearch = () => {
+    setSearch(text);
+  };
+  const filteredUsers = useMemo(
+    () =>
+      users.filter((user) => {
+        console.log("filtering users");
+        return user.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    [search]
+  );
   return (
     <div>
-      Hello
-      <input
-        type="text"
-        value={username}
-        ref={textInput}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <div>Foo: {fooRef.current}</div>
-      <button onClick={onTextFocus}>Focus on input pls</button>
+      <input type="text" value={text} onChange={handleText} />
+      <button onClick={handleSearch}>Search</button>
+      <ul>
+        {filteredUsers.map((filteredUser) => (
+          <li key={filteredUser.id}>{filteredUser.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
