a15e5d55194d21e769cb55d2a558f108f3d7393a
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-07 00:15:51 +0100

    51 use state hook

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
-import React, { useState, useCallback } from "react";
-
-const initialUsers = [
-  {
-    id: "1",
-    name: "foo",
-  },
-  {
-    id: "2",
-    name: "bar",
-  },
-];
-
-const List = React.memo(({ users, onRemove }) => {
-  console.log("rendering list");
-  return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.name} <span onClick={() => onRemove(user.id)}>X</span>
-        </li>
-      ))}
-    </ul>
-  );
-});
+import { useState } from "react";
 
 const App = () => {
-  const [users, setUsers] = useState(initialUsers);
-  const [text, setText] = useState("");
-  const handleRemove = useCallback(
-    (userId) => {
-      console.log("handleRemove", userId);
-      const filteredUsers = users.filter((user) => user.id !== userId);
-      setUsers(filteredUsers);
-    },
-    [users]
-  );
-  const handleText = (event) => {
-    setText(event.target.value);
+  const [count, setCount] = useState(() => {
+    console.log("im called once");
+    return 0;
+  });
+  const [name, setName] = useState("");
+  console.log("render", count, name);
+  const increaseCounter = () => {
+    console.log("increaseCounter");
+    setCount(count + 1);
   };
+
   return (
     <div>
-      <input type="text" value={text} onChange={handleText} />
-      <List users={users} onRemove={handleRemove} />
+      <h1>Hello React {count}</h1>
+      <button onClick={increaseCounter}>Click me</button>
     </div>
   );
 };
