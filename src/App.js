297fd871148fda7bae4fc63386afde83225389aa
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-06 21:42:05 +0100

    57-use-callback-hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import { useState, useMemo } from "react";
-const users = [
+import React, { useState, useCallback } from "react";
+
+const initialUsers = [
   {
     id: "1",
-    name: "Foo",
+    name: "foo",
   },
   {
     id: "2",
     name: "bar",
   },
 ];
-const App = () => {
-  const [text, setText] = useState("");
-  const [search, setSearch] = useState("");
-  console.log("text", text);
-  console.log("search", search);
-  const handleText = (e) => {
-    setText(e.target.value);
-  };
-  const handleSearch = () => {
-    setSearch(text);
-  };
-  const filteredUsers = useMemo(
-    () =>
-      users.filter((user) => {
-        console.log("filtering users");
-        return user.name.toLowerCase().includes(search.toLowerCase());
-      }),
-    [search]
+
+const List = React.memo(({ users, onRemove }) => {
+  console.log("rendering list");
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.name} <span onClick={() => onRemove(user.id)}>X</span>
+        </li>
+      ))}
+    </ul>
   );
+});
+
+const App = () => {
+  const [users, setUsers] = useState(initialUsers);
+  const [text, setText] = useState("");
+  const handleRemove = useCallback(
+    (userId) => {
+      console.log("handleRemove", userId);
+      const filteredUsers = users.filter((user) => user.id !== userId);
+      setUsers(filteredUsers);
+    },
+    [users]
+  );
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
   return (
     <div>
       <input type="text" value={text} onChange={handleText} />
-      <button onClick={handleSearch}>Search</button>
-      <ul>
-        {filteredUsers.map((filteredUser) => (
-          <li key={filteredUser.id}>{filteredUser.name}</li>
-        ))}
-      </ul>
+      <List users={users} onRemove={handleRemove} />
     </div>
   );
 };
