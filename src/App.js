52aeffd07ae2635d41984615c9944ae884677d20
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-07 20:50:59 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useState } from "react";
-
 const App = () => {
-  const [count, setCount] = useState(() => {
-    console.log("im called once");
-    return 0;
-  });
-  const [name, setName] = useState("");
-  console.log("render", count, name);
-  const increaseCounter = () => {
-    console.log("increaseCounter");
-    setCount(count + 1);
-  };
-
   return (
     <div>
-      <h1>Hello React {count}</h1>
-      <button onClick={increaseCounter}>Click me</button>
+      <h1>Hello React</h1>
     </div>
   );
 };
