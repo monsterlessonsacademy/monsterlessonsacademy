c59ceb8f8373818512c7c02efdda6d410735973b
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-12 20:36:26 +0100

    WIP

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-var someElement = document.querySelector(".foo");
-someElement.addEventListener("blur", function (event) {
-    var target = event.target;
-    console.log("event", target.value);
-});
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-const someElement = document.querySelector(".foo");
-
-someElement.addEventListener("blur", (event) => {
-  const target = event.target as HTMLInputElement;
-  console.log("event", target.value);
-});
