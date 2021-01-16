f7067390cc04cad9e6ad0b9144fa4fc74696148a
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-16 23:23:19 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import FetchWithClasses from "./FetchWithClasses";
+import FetchWithHooks from "./FetchWithHooks";
 
 const App = () => {
   return (
     <div>
       <h1>Hello monsterlessons</h1>
       <FetchWithClasses />
+      <FetchWithHooks />
     </div>
   );
 };
