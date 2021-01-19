7d4f8b0b38f57cf4ca3333ead73a8c0459faf563
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-19 23:53:42 +0100

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-var a = "a";
+const reducer = (state = [], action) => {
+  if (action.type === "ADD_USER") {
+    return [...state, action.payload];
+  }
+
+  return state;
+};
+
+const store = Redux.createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "ADD_USER", payload: "jack" });
+store.dispatch({ type: "ADD_USER", payload: "john" });
