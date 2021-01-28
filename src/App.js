ba087cd0cc4b33547802374d508a9fd81acf46a1
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-01-28 22:24:27 +0100

    81

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Modal from "./Modal";
+import { useState } from "react";
+import "./app.css";
+
 const App = () => {
-  return <h1>Hello monsterlessons</h1>;
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  console.log("isModalOpened", isModalOpened);
+  return (
+    <div>
+      App component
+      <button onClick={() => setIsModalOpened(true)}>Open modal</button>
+      <Modal isOpened={isModalOpened} onClose={() => setIsModalOpened(false)}>
+        <form onSubmit={() => setIsModalOpened(false)} className="login-form">
+          <section>
+            <label>Email</label>
+            <input type="email" />
+          </section>
+          <section>
+            <label>Password</label>
+            <input type="password" />
+          </section>
+          <button>Sign In</button>
+        </form>
+      </Modal>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,24 @@
+import { createPortal } from "react-dom";
+import "./modal.css";
+
+const Modal = ({ isOpened, children, onClose }) => {
+  if (!isOpened) {
+    return null;
+  }
+  return createPortal(
+    <div>
+      <div className="overlay"></div>
+      <div className="modal">
+        <div>
+          <span className="close-button" onClick={onClose}>
+            X
+          </span>
+          <div className="modal-content">{children}</div>
+        </div>
+      </div>
+    </div>,
+    document.getElementById("modal")
+  );
+};
+
+export default Modal;
