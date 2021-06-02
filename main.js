9117945dedcb6ad9d15250ccb6881e6be6bfa912
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-06-02 21:07:23 +0200

    117 this context in javascript

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-var a = "a";
+// function getItem() {
+//   console.log(this);
+// }
+
+// getItem();
+
+// const item = {
+//   title: "Ball",
+//   getItem() {
+//     console.log(this.title);
+//   },
+// };
+
+// item.getItem();
+
+class Item {
+  title = "Ball";
+
+  getItem() {
+    [1, 2, 3].map((item) => {
+      console.log(this);
+    });
+    // function someFn() {
+    //   console.log(this);
+    // }
+    // someFn();
+  }
+}
+
+const item = new Item();
+item.getItem();
