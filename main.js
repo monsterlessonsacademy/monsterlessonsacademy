aadef0adae8e717997734460d00b24b50a75c653
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-09-09 14:23:11 +0200

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-var a = "a";
+const memoizedAdd = () => {
+  let cache = {};
+  return (value) => {
+    if (value in cache) {
+      console.log("Fetching from cache");
+      return cache[value];
+    } else {
+      console.log("Calculating result");
+      let result = value + 10;
+      cache[value] = result;
+      return result;
+    }
+  };
+};
+// returned function from memoizedAdd
+const newAdd = memoizedAdd();
+console.log(newAdd(9)); //output: 19 calculated
+console.log(newAdd(9)); //output: 19 cached
