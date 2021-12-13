8543ad231a39e23ef1ffa05bb1f4bd75e0492002
Oleksandr Kocherhin <oko@facelift-bbt.com>
2021-12-13 21:41:02 +0100

    178

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ io.on("connection", (socket) => {
   console.log("connected");
   socket.on("login", (name) => {
     console.log("login", name, socket.id);
+    socket.broadcast.emit("new-login", name);
+  });
+  socket.on("send-to", (params) => {
+    console.log("send-to", params);
+    socket.to(params.recipient).emit("private-message", {
+      message: params.message,
+      sender: socket.id,
+    });
+  });
+  socket.on("join-room", (roomName) => {
+    console.log("join", roomName);
+    socket.join(roomName);
+    io.to(socket.id).emit("joined-room");
+    io.to(roomName).emit("public-message", `New user ${socket.id}`);
   });
 });
 
