d8d63e50bb51074108ed18d409c164a7c6f68906
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-05-10 17:38:23 +0200

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var a = "a";
+const $toggler = document.querySelector(".toggler");
+const $sidebar = document.querySelector(".sidebar");
+const $main = document.querySelector(".main");
+const $closeSidebarButton = document.querySelector(".closeSidebarButton");
+
+$closeSidebarButton.addEventListener("click", () => {
+  $sidebar.classList.remove("is-opened");
+});
+
+$toggler.addEventListener("click", () => {
+  $sidebar.classList.toggle("is-opened");
+});
