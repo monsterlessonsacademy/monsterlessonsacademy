8087d35f664b90c9d8452ef1192d7b9c4bd5bd3c
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-06-24 21:06:57 +0200

    230

--- main.js
+++ main.js
@@ -1,52 +1,16 @@
-const typeOf = (input) => {
-  const rawObject = Object.prototype.toString.call(input).toLowerCase();
-  const typeOfRegex = /\[object (.*)]/g;
-  const type = typeOfRegex.exec(rawObject)[1];
-  return type;
+const clone = (input) => {
+  if (input === null || typeof input !== "object") {
+    return input;
+  }
+
+  const initialOutput = Array.isArray(input) ? [] : {};
+
+  return Object.keys(input).reduce((acc, key) => {
+    acc[key] = clone(input[key]);
+    return acc;
+  }, initialOutput);
 };
 
-const shallowCompare = (source, target) => {
-  if (typeOf(source) !== typeOf(target)) {
-    return false;
-  }
-
-  if (typeOf(source) === "array") {
-    if (source.length !== target.length) {
-      return false;
-    }
-    return source.every((el, index) => el === target[index]);
-  } else if (typeOf(source) === "object") {
-    if (Object.keys(source).length !== Object.keys(target).length) {
-      return false;
-    }
-    return Object.keys(source).every((key) => source[key] === target[key]);
-  } else if (typeOf(source) === "date") {
-    return source.getTime() === target.getTime();
-  }
-
-  return source === target;
-};
-
-const deepCompare = (source, target) => {
-  if (typeOf(source) !== typeOf(target)) {
-    return false;
-  }
-
-  if (typeOf(source) === "array") {
-    if (source.length !== target.length) {
-      return false;
-    }
-    return source.every((el, index) => deepCompare(el, target[index]));
-  } else if (typeOf(source) === "object") {
-    if (Object.keys(source).length !== Object.keys(target).length) {
-      return false;
-    }
-    return Object.keys(source).every((key) =>
-      deepCompare(source[key], target[key])
-    );
-  } else if (typeOf(source) === "date") {
-    return source.getTime() === target.getTime();
-  }
-
-  return source === target;
-};
+const a = { b: { c: 1 } };
+const newA = clone(a);
+newA.b.d = 2;
