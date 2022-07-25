213b6c527629b88b933c12645c83cb8385135124
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-07-25 15:20:36 +0200

    eslint

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-console.log(" prettier111");
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
