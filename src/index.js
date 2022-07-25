48f1e683f651e3e3372e34fcf59796d1d0047758
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-07-25 14:47:15 +0200

    Added logs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+console.log(" prettier");
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
