506c94ef9ed08eb599325aa5abae1e7624fd8ca6
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-07-26 11:32:25 +0200

    243

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
-import CreatableSelect from "react-select/creatable";
-
+import RechartsExample from "./RechartsExample";
+import ChartJsExample from "./ChartJsExample";
 const App = () => {
-  const options = [
-    { value: "jack", label: "Jack", color: "#FF8B00" },
-    { value: "john", label: "John", color: "#36B37E" },
-    { value: "mike", label: "Mike", color: "#0052CC" },
-  ];
-  const colorStyles = {
-    control: (styles) => ({ ...styles, backgroundColor: "white" }),
-    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-      return { ...styles, color: data.color };
-    },
-    multiValue: (styles, { data }) => {
-      return {
-        ...styles,
-        backgroundColor: data.color,
-        color: "#fff",
-      };
-    },
-    multiValueLabel: (styles, { data }) => {
-      return {
-        ...styles,
-        color: "#fff",
-      };
-    },
-    multiValueRemove: (styles, { data }) => {
-      return {
-        ...styles,
-        color: "#fff",
-        cursor: "pointer",
-        ":hover": {
-          color: "#fff",
-        },
-      };
-    },
-  };
-  const handleChange = (selectedOption, actionMeta) => {
-    console.log("handleChange", selectedOption, actionMeta);
-  };
-  const handleInputChange = (inputValue, actionMeta) => {
-    console.log("handleInputChange", inputValue, actionMeta);
-  };
   return (
-    <CreatableSelect
-      options={options}
-      onChange={handleChange}
-      onInputChange={handleInputChange}
-      isMulti
-      styles={colorStyles}
-    />
+    <div>
+      <h1>Monsterlessons Academy</h1>
+      <RechartsExample />
+      <ChartJsExample />
+    </div>
   );
 };
 
