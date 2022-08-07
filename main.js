3bbcc366c72f482f56a65ea4f2bc3ca1123b00cd
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-08-07 14:09:21 +0200

    248

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-var a = "a";
+// const regexr = new RegExp('abc')
+const regexr = /abc/;
+// const result = "Foo bar baz foo".replace(/foo/gi, "test");
+// const result = "Foo bar foo baz".split(/foo/gi);
+// const result = "Foo bar foo baz".match(/foo/gi);
+console.log("result", result);
