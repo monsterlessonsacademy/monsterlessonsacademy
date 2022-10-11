6beba0dcaf656c1b3dcc2fc030161b0056b0b9b5
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-10-11 21:37:30 +0200

    WIP

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require("ignore-styles");
 
 require("@babel/register")({
-  ignore: [/(node_module)/],
+  ignore: [/(node_modules)/],
   presets: ["@babel/preset-env", "@babel/preset-react"],
 });
 
