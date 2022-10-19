2c2d17925f4c4ad0007883cbb0cf5968c17279d9
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-10-19 14:33:50 +0200

    263

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
-const container = document.getElementById("root");
-const root = createRoot(container);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   </React.StrictMode>
 );
