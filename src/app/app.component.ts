565ec578736993d15713ad04226ce3733c9c6178
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-11-15 21:43:59 +0100

    274

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   registerForm = this.fb.group({
     username: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
     roleId: [1, Validators.required],
   });
   isSubmitted = false;
@@ -23,12 +23,16 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm.get('roleId')?.valueChanges.subscribe((roleId) => {
-      console.log('roleId', roleId);
+      console.log('SEND API REQUEST AND UPDATE ROLE', roleId);
     });
   }
 
   onSubmit(): void {
-    console.log('onSubmit', this.registerForm.value);
+    console.log(
+      'submitted form',
+      this.registerForm.value,
+      this.registerForm.invalid
+    );
     this.isSubmitted = true;
   }
 }
