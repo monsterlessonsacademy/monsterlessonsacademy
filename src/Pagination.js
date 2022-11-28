b4c0a549fb13d6468dd0def07d0212bbd39e8183
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-11-28 21:31:40 +0100

    WIP

--- src/Pagination.js
+++ src/Pagination.js
@@ -2,7 +2,26 @@ import "./pagination.css";
 import classNames from "classnames";
 
 const range = (start, end) => {
-  return [...Array(end).keys()].map((el) => el + start);
+  return [...Array(end - start + 1).keys()].map((el) => el + start);
+};
+
+const getPagesCut = ({ pagesCount, pagesCutCount, currentPage }) => {
+  const ceiling = Math.ceil(pagesCutCount / 2);
+  const floor = Math.floor(pagesCutCount / 2);
+
+  if (pagesCount < pagesCutCount) {
+    console.log("case 1");
+    return { start: 0, end: pagesCount };
+  } else if (currentPage >= 1 && currentPage <= ceiling) {
+    console.log("case 2");
+    return { start: 0, end: pagesCutCount };
+  } else if (currentPage + floor >= pagesCount) {
+    console.log("case 3");
+    return { start: pagesCount - pagesCutCount, end: pagesCount };
+  } else {
+    console.log("case 4");
+    return { start: currentPage - ceiling, end: currentPage + floor };
+  }
 };
 
 const PaginationItem = ({ page, currentPage, onPageChange, isDisabled }) => {
@@ -20,9 +39,15 @@ const PaginationItem = ({ page, currentPage, onPageChange, isDisabled }) => {
 
 const Pagination = ({ currentPage, total, limit, onPageChange }) => {
   const pagesCount = Math.ceil(total / limit);
-  const pages = range(1, pagesCount);
   const isFirstPage = currentPage === 1;
   const isLastPage = currentPage === pagesCount;
+  const pagesCut = getPagesCut({
+    pagesCount,
+    pagesCutCount: 3,
+    currentPage,
+  });
+  const pages = range(pagesCut.start, pagesCut.end);
+  console.log("pagesCut", pagesCut, pagesCount, pages);
   return (
     <ul className="pagination">
       <PaginationItem
