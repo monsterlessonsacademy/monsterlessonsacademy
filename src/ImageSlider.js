82238e62890fa560dca4eb4260825e765fe4921f
Oleksandr Kocherhin <oko@facelift-bbt.com>
2022-11-29 14:00:48 +0100

    WIP

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const slideStyles = {
   width: "100%",
@@ -47,17 +47,21 @@ const dotStyle = {
 };
 
 const ImageSlider = ({ slides }) => {
+  const timerRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
-  const goToNext = () => {
+
+  const goToNext = useCallback(() => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
-  };
+  }, [currentIndex, slides]);
+
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
@@ -65,6 +69,18 @@ const ImageSlider = ({ slides }) => {
     ...slideStyles,
     backgroundImage: `url(${slides[currentIndex].url})`,
   };
+
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = setTimeout(() => {
+      goToNext();
+    }, 2000);
+
+    return () => clearTimeout(timerRef.current);
+  }, [goToNext]);
 
   return (
     <div style={sliderStyles}>
