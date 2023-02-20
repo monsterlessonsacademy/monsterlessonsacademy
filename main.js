8ded579774fab9a160bae8cf1a262f7533de0d54
Oleksandr Kocherhin <oko@facelift-bbt.com>
2023-02-20 21:19:42 +0100

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import "./style.css";
+const basicConfig = { apiUrl: "http://google.com/api", port: 3000 };
+
+const createExtendedConfig = (config) => {
+  return { ...config, host: "google.com" };
+};
+
+const extendedConfig = createExtendedConfig(basicConfig);
+
+console.log(basicConfig, extendedConfig);
