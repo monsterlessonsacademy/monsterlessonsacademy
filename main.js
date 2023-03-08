55f6556eacb84ebb7d0dfe3f25869e6975f707de
Oleksandr Kocherhin <oko@facelift-bbt.com>
2023-03-08 21:32:06 +0100

    312

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import "./style.css";
+console.log(0 || "not found");
+console.log("" || "not found");
+console.log(false || "not found");
+console.log(undefined || "not found");
+console.log(null || "not found");
+
+console.log(0 ?? "not found");
+console.log("" ?? "not found");
+console.log(false ?? "not found");
+console.log(undefined ?? "not found");
+console.log(null ?? "not found");
