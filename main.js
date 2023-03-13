c646bc022db5ba6eabe95676ce01a9361fb152f2
Oleksandr Kocherhin <oko@facelift-bbt.com>
2023-03-13 11:35:57 +0100

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-import "./style.css";
+const doSetTimeout = (i) => {
+  setTimeout(() => {
+    console.log(i);
+  }, 0);
+};
+
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 0);
+// }
+
+for (var i = 0; i < 10; i++) {
+  doSetTimeout(i);
+}
