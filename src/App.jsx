68f5949ae2619952027655a5942eec7653d9e10f
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-05-09 16:00:31 +0200

    337

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
-import useLocalStorage from "./useLocalStorage";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./app.css";
+
+const getUsers = async ({ pageParam = 0 }) => {
+  console.log("getUsers", pageParam);
+  const res = await fetch(
+    `https://api.realworld.io/api/articles?limit=10&offset=${pageParam}`
+  );
+  const data = await res.json();
+  return { ...data, prevOffset: pageParam };
+};
 
 const App = () => {
-  const [name, setName] = useLocalStorage("name", "Jack");
-  console.log(name);
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["users"],
+    queryFn: getUsers,
+    getNextPageParam: (lastPage) => {
+      console.log(
+        "getNextPageParam",
+        lastPage.prevOffset + 10 > lastPage.articlesCount / 10,
+        lastPage.prevOffset,
+        lastPage.articlesCount / 10
+      );
+      if (lastPage.prevOffset + 10 > lastPage.articlesCount) {
+        return false;
+      }
+      return lastPage.prevOffset + 10;
+    },
+  });
+  const articles = data?.pages.reduce((acc, page) => {
+    return [...acc, ...page.articles];
+  }, []);
 
   return (
     <div>
       <h1>Hello monsterlessons</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
+
+      <InfiniteScroll
+        dataLength={articles ? articles.length : 0}
+        next={() => fetchNextPage()}
+        hasMore={hasNextPage}
+        loader={<div>Loading...</div>}
+      >
+        <div>
+          {articles &&
+            articles.map((article, index) => (
+              <div key={index} className="element">
+                {article.title}
+              </div>
+            ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
