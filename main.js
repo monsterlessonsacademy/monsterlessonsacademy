eff755c1fc6d8b8276f5065218c654543fcbd4a1
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-05-25 22:18:30 +0200

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-import "./style.css";
+const testUrl = "https://www.example.com/page?param1=value1&param2=value2";
+const url = new URL(testUrl);
+const params = Object.fromEntries(url.searchParams.entries());
+const searchParams = new URLSearchParams(params);
+const newUrl = `http://localhost:3000?${searchParams.toString()}`;
+console.log(newUrl);
