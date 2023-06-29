e7870cc24f0be14155ebabc950c62f7462f6a225
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-06-29 22:29:21 +0200

    WIP

--- main.js
+++ main.js
@@ -1,36 +1,16 @@
 const obj = { foo: "foo", bar: "bar" };
+// console.log(obj.baz?.baq);
 const map = new Map([
   ["foo", "foo"],
   ["bar", "bar"],
+  [1, "1"],
 ]);
-map.set("foo", "foo");
-map.set("bar", "bar");
-map.set("1", "111");
-
-const newMap = new Map(map);
-const merged = new Map([...map, ...newMap]);
-
-for (const key in obj) {
-  if (obj.hasOwnProperty(key)) {
-    console.log(key);
-  }
-}
-
-for (const [key, value] of map) {
-  console.log(key, value);
-}
-
-console.log(
-  "!!",
-  map.has(1),
-  map.size,
-  map.keys(),
-  map.values(),
-  merged.get("foo")
-);
-
-Array.from(map, ([key, value]) => console.log("QQQ", key, value));
-Array.from(map.values(), (value) => console.log("EWWWW", value));
-
-console.log("fy", JSON.stringify(Array.from(map.entries())));
-console.log("fy", JSON.stringify(Object.fromEntries(map)));
+map.set("baz", "baz");
+// console.log(map.size);
+// for (const [key, value] of map) {
+//   console.log(key, value);
+// }
+// const obj2 = {...obj}
+// console.log(JSON.stringify(Object.fromEntries(map)));
+const result = Array.from(map, ([key, value]) => `${key} ${value}`);
+console.log(result);
