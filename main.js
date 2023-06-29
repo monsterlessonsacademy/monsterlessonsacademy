079017e21b41e0a6a98198d6beaeec4d5b8e13ba
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-06-29 11:08:26 +0200

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-import "./style.css";
+const obj = { foo: "foo", bar: "bar" };
+const map = new Map([
+  ["foo", "foo"],
+  ["bar", "bar"],
+]);
+map.set("foo", "foo");
+map.set("bar", "bar");
+map.set(1, "111");
+
+const newMap = new Map(map);
+const merged = new Map([...map, ...newMap]);
+
+for (const key in obj) {
+  if (obj.hasOwnProperty(key)) {
+    console.log(key);
+  }
+}
+
+for (const [key, value] of map) {
+  console.log(key, value);
+}
+
+console.log(
+  "!!",
+  map.has(1),
+  map.size,
+  map.keys(),
+  map.values(),
+  merged.get("foo")
+);
+
+Array.from(map, ([key, value]) => console.log("QQQ", key, value));
+Array.from(map.values(), (value) => console.log("EWWWW", value));
