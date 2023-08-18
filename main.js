0d7b4cdf8f28f0b0d25de46d6378025bf78a019f
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-08-18 13:48:08 +0200

    Added singly

--- main.js
+++ main.js
@@ -1,38 +1,92 @@
-class GoodQueue {
-  items = {};
-  tail = 0;
-  head = 0;
-
-  enqueue(element) {
-    this.items[this.tail] = element;
-    this.tail++;
-  }
-
-  dequeue() {
-    const item = this.items[this.head];
-    delete this.items[this.head];
-    this.head++;
-    return item;
-  }
-
-  isEmpty() {
-    return this.size() === 0;
-  }
-
-  size() {
-    return this.tail - this.head;
-  }
-
-  peek() {
-    return this.items[this.head];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-const goodQueue = new GoodQueue();
-goodQueue.enqueue({ id: "1", name: "foo" });
-goodQueue.enqueue({ id: "2", name: "bar" });
-goodQueue.enqueue({ id: "3", name: "baz" });
-console.log(goodQueue.size());
-console.log(goodQueue.isEmpty());
-const firstElement = goodQueue.dequeue();
-console.log(firstElement, goodQueue.items);
+class SinglyLinkedList {
+  constructor() {
+    this.size = 0;
+    this.head = null;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+
+    if (this.size === 0) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+
+      currentNode.next = newNode;
+    }
+
+    this.size++;
+  }
+
+  addByIndex(index, value) {
+    if (this.size === 0) {
+      this.add(value);
+      return;
+    }
+
+    const newNode = new Node(value);
+    let currentNode = this.head;
+    let previousNode;
+    let position = 0;
+
+    while (position < index) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+      position++;
+    }
+
+    newNode.next = currentNode;
+    previousNode.next = newNode;
+  }
+
+  getByIndex(index) {
+    let position = 0;
+    let currentNode = this.head;
+
+    while (position < index) {
+      currentNode = currentNode.next;
+      position++;
+    }
+
+    return currentNode;
+  }
+
+  removeByIndex(index) {
+    let currentNode = this.head;
+    let previousNode;
+    let position = 0;
+    if (index === 0) {
+      this.head = currentNode.next;
+    } else {
+      while (position < index) {
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+        position++;
+      }
+
+      previousNode.next = currentNode.next;
+    }
+
+    this.size--;
+  }
+}
+
+const singly = new SinglyLinkedList();
+singly.add("a");
+singly.add("b");
+singly.add("c");
+console.log(singly);
+console.log(singly.getByIndex(0));
+// singly.removeByIndex(1);
+singly.addByIndex(1, "f");
+console.log(singly);
