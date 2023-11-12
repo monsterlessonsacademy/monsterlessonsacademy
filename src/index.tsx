0de5dd16b3751fde7af2ee2ab7555a0a7d82ecbe
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-11-12 20:49:20 +0100

    403

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,2 @@
 export * from "./button";
 export * from "./input";
-
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
-      name: "mla-library",
-
+      name: "mla-comps",
       fileName: "index",
     },
     rollupOptions: {
