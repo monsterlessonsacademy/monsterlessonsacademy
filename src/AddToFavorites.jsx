0c1ee74dd1ac0ccf34364df545e026c4f62d6884
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-11-14 09:47:07 +0100

    409

--- src/AddToFavorites.jsx
+++ src/AddToFavorites.jsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
-
-const addToFavoritesApi = (method, favoritesCount) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        id: "1",
-        title: "foo",
-        isFavorited: method === "delete" ? false : true,
-        favoritesCount:
-          method === "delete" ? favoritesCount - 1 : favoritesCount + 1,
-      });
-    }, 2000);
-  });
-};
+import axios from "axios";
 
 const AddToFavorites = ({ isFavorited, favoritesCount, articleId }) => {
-  // const apiUrl = `/articles/${articleSlug}/favorite`;
-  const [isFavoritedInternal, setIsFavoritedInterface] = useState(isFavorited);
+  const [isFavoritedInternal, setIsFavoritedInternal] = useState(isFavorited);
   const [favoritesCountInternal, setFavoritesCountInternal] = useState(
     favoritesCount
   );
-  // const isFavoritedWithResponse = response
-  //   ? response.article.favorited
-  //   : isFavorited;
-  // const favoritesCountWithResponse = response
-  //   ? response.article.favoritesCount
-  //   : favoritesCount;
   const buttonClasses = classNames({
     btn: true,
     "btn-sm": true,
@@ -35,17 +15,20 @@ const AddToFavorites = ({ isFavorited, favoritesCount, articleId }) => {
   });
   const handleLike = (event) => {
     event.preventDefault();
-    fetch(`http://localhost:3004/articles/${articleId}`, {
-      method: "patch",
-      mode: "no-cors",
-      credentials: "include",
-      body: JSON.stringify({
-        isFavorited: isFavorited ? false : true,
-        favoritesCount: isFavorited ? favoritesCount - 1 : favoritesCount + 1,
-      }),
-    }).then((response) => {
-      console.log("response", response);
-    });
+    axios
+      .patch(`http://localhost:3004/articles/${articleId}`, {
+        isFavorited: isFavoritedInternal ? false : true,
+        favoritesCount: isFavoritedInternal
+          ? favoritesCountInternal - 1
+          : favoritesCountInternal + 1,
+      })
+      .then((response) => {
+        // Simulating slow backend
+        setTimeout(() => {
+          setFavoritesCountInternal(response.data.favoritesCount);
+          setIsFavoritedInternal(response.data.isFavorited);
+        }, 2000);
+      });
   };
 
   return (
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import AddToFavorites from "./AddToFavorites";
+import axios from "axios";
 
 const App = () => {
+  const [article, setArticle] = useState(null);
+  useEffect(() => {
+    axios.get("http://localhost:3004/articles/1").then((response) => {
+      setArticle(response.data);
+    });
+  }, []);
   return (
     <Fragment>
       <h1>Monsterlessons Academy</h1>
-      <AddToFavorites isFavorited={false} favoritesCount={10} articleId="1" />
+      {article && (
+        <AddToFavorites
+          isFavorited={article.isFavorited}
+          favoritesCount={article.favoritesCount}
+          articleId="1"
+        />
+      )}
     </Fragment>
   );
 };
