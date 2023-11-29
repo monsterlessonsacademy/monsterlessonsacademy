770eff6979e3307ff0dafc6c636f2dabc6385d4e
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-11-29 13:31:00 +0100

    414 wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ChildComponent } from './child.component';
+import { ProgressiveImage } from './progressiveImage/progressiveImage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ChildComponent],
+  imports: [CommonModule, RouterOutlet, ProgressiveImage],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  isVisible = false;
-  prefetchCondition = false;
-}
+export class AppComponent {}
