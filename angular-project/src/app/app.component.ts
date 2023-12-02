0a09342a9a67f7ab662c15553869c4df62ffd608
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-12-02 22:35:22 +0100

    WIP

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -7,20 +7,15 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 import * as LR from '@uploadcare/blocks';
+
 // LR.FileUploaderRegular.shadowStyles = `
-//   :host {
-//     --darkmode: 1;
-//   }
 //   :host lr-simple-btn button {
 //     background: teal;
 //     color: white;
 //   }
-//   :host lr-simple-btn button:hover {
-//     background: teal;
-//   }
 // `;
+
 LR.registerBlocks(LR);
 
 @Component({
@@ -32,24 +27,26 @@ LR.registerBlocks(LR);
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('ctxProvider', { static: true }) ctxProviderRef!: ElementRef<
+  @ViewChild('ctxProvider', { static: true }) ctxProvider!: ElementRef<
     typeof LR.UploadCtxProvider.prototype
   >;
   uploadedFiles: LR.OutputFileEntry[] = [];
 
   ngOnInit(): void {
-    this.ctxProviderRef.nativeElement.addEventListener(
+    this.ctxProvider.nativeElement.addEventListener(
       'data-output',
       this.handleUploadEvent
     );
-    this.ctxProviderRef.nativeElement.addEventListener(
+    this.ctxProvider.nativeElement.addEventListener(
       'done-flow',
       this.handleDoneFlow
     );
   }
 
   handleUploadEvent = (e: Event) => {
-    if (!(e instanceof CustomEvent)) return;
+    if (!(e instanceof CustomEvent)) {
+      return;
+    }
 
     if (e.detail) {
       this.uploadedFiles = e.detail as LR.OutputFileEntry[];
@@ -59,8 +56,4 @@ export class AppComponent implements OnInit {
   handleDoneFlow = () => {
     console.log('handleDoneFlow');
   };
-
-  handleRemoveClick(uuid?: string) {
-    console.log('handleRemoveClick', uuid);
-  }
 }
