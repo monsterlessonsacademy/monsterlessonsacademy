16eaba0846cbaf8b99f771b0d1a106a108d4af97
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-12-14 17:35:00 +0100

    420

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,18 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
+import en from "../i18n/en/translation.json";
+import de from "../i18n/de/translation.json";
 
 i18n
   .use(initReactI18next)
-  .use(HttpBackend)
+  // .use(HttpBackend)
   .init({
     debug: true,
     fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    backend: {
-      loadPath: "http://localhost:3005/locales/{{lng}}/{{ns}}.json",
+    // backend: {
+    //   loadPath: "http://localhost:3005/locales/{{lng}}/{{ns}}.json",
+    // },
+    resources: {
+      en: {
+        translation: en,
+      },
+      de: {
+        translation: de,
+      },
     },
     // language resources
     // resources: {
