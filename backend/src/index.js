82a366c98fc8bf8b0b514c01379e62c3818ebb2b
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-12-18 15:01:16 +0100

    420

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const app = express();
-app.use(cors());
-app.use("/locales", express.static("locales"));
-app.use("/locales", express.static(path.join(__dirname, "locales")));
-app.listen(3005);
--- frontend/src/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import { useTranslation } from "react-i18next";
-
-const Dashboard = () => {
-  const { t, i18n } = useTranslation();
-  return <h2>{t("news")}</h2>;
-};
-
-export default Dashboard;
