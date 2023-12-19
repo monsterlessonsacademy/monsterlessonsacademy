48859fa5259f4e6d459def1ce3b01f923020ff92
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2023-12-19 21:34:14 +0100

    424

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Button from "./Button";
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  override: {
+    backgroundColor: "black",
+    color: "white",
+  },
+});
 
 const App = () => {
   return (
     <div>
       <h1>Monsterlessons Academy</h1>
-      <Button text="foo" />
+      <Button
+        text="foo"
+        isHighlighted
+        variant="danger"
+        style={styles.override}
+      />
     </div>
   );
 };
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,36 +1,49 @@
 import * as stylex from "@stylexjs/stylex";
 
-const fadeIn = stylex.keyframes({
-  from: { opacity: 0 },
-  to: { opacity: 1 },
-});
-
 const styles = stylex.create({
   base: {
     fontSize: 18,
-    color: "white",
     backgroundColor: {
       default: "teal",
       ":hover": "blue",
     },
+    color: "white",
     width: {
       default: "100px",
       "@media (max-width: 800px)": "100%",
     },
-    animationName: fadeIn,
-    animationDuration: "1s",
   },
   highlighted: {
-    color: "purple",
+    backgroundColor: "orange",
+  },
+  danger: {
+    backgroundColor: "red",
+  },
+  primary: {
+    backgroundColor: "green",
   },
 });
 
 interface ButtonProps {
   text: string;
+  isHighlighted: boolean;
+  variant: "danger" | "primary";
+  style: stylex.StyleXStyles;
 }
 
-const Button = ({ text }: ButtonProps) => {
-  return <button {...stylex.props(styles.base)}>{text}</button>;
+const Button = ({ text, isHighlighted, variant, style }: ButtonProps) => {
+  return (
+    <button
+      {...stylex.props(
+        styles.base,
+        isHighlighted && styles.highlighted,
+        styles[variant],
+        style
+      )}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
