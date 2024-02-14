51e55de72d383c056a44ba5180cdd55e9a70572d
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-02-14 08:33:56 +0100

    445

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+const getData = (data: string): void => {};
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const data: string | undefined = undefined;
+
+getData(data ?? "");
