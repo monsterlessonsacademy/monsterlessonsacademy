ac4f68556178bd44acc9009264277791f6f2388a
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-04-05 14:46:15 +0200

    472

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PaginationComponent } from './pagination.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PaginationComponent],
+  imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  currentPage = 1;
-  changePage(page: number): void {
-    this.currentPage = page;
-    console.log('changed current page to ', page);
-  }
-
-  itemsChange(items: string[]): void {
-    console.log('parent itemsChange', items);
-  }
+  meetings = {
+    '2024-04-05': ['Drink Coffee', 'Learn React', 'Sleep'],
+    '2024-04-06': ['Drink Coffee', 'Learn Angular', 'Sleep'],
+  };
 }
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,71 @@
+import {
+  Component,
+  InputSignal,
+  Signal,
+  WritableSignal,
+  computed,
+  input,
+  signal,
+} from '@angular/core';
+import { Meetings } from './meetings.interface';
+import { DateTime, Info, Interval } from 'luxon';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css'],
+  imports: [CommonModule],
+  standalone: true,
+})
+export class CalendarComponent {
+  meetings: InputSignal<Meetings> = input.required();
+  today: WritableSignal<DateTime> = signal(DateTime.local());
+  activeDay: WritableSignal<DateTime | null> = signal(null);
+  activeDayMeetings: Signal<string[]> = computed(() => {
+    const activeDay = this.activeDay();
+    if (activeDay === null) {
+      return [];
+    }
+
+    const activeDayISO = activeDay.toISODate();
+
+    if (!activeDayISO) {
+      return [];
+    }
+
+    return this.meetings()[activeDayISO] ?? [];
+  });
+  firstDayOfActiveMonth: WritableSignal<DateTime> = signal(
+    this.today().startOf('month'),
+  );
+  weekDays: Signal<string[]> = signal(Info.weekdays('short'));
+  daysOfMonth: Signal<DateTime[]> = computed(() => {
+    return Interval.fromDateTimes(
+      this.firstDayOfActiveMonth().startOf('week'),
+      this.firstDayOfActiveMonth().endOf('month').endOf('week'),
+    )
+      .splitBy({ day: 1 })
+      .map((d) => {
+        if (d.start === null) {
+          throw new Error('Wrong dates');
+        }
+        return d.start;
+      });
+  });
+  DATE_MED = DateTime.DATE_MED;
+
+  goToPreviousMonth(): void {
+    this.firstDayOfActiveMonth.set(
+      this.firstDayOfActiveMonth().minus({ month: 1 }),
+    );
+  }
+  goToNextMonth(): void {
+    this.firstDayOfActiveMonth.set(
+      this.firstDayOfActiveMonth().plus({ month: 1 }),
+    );
+  }
+  goToToday(): void {
+    this.firstDayOfActiveMonth.set(this.today().startOf('month'));
+  }
+}
