690c6d2407435d0e93a70651fbaab4a866bbecab
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-04-27 16:14:50 +0200

    4756

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,73 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { PaginationComponent } from './pagination.component';
+import { Observable, map, of } from 'rxjs';
+
+// export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+//   return (control: AbstractControl): ValidationErrors | null => {
+//     const forbidden = nameRe.test(control.value);
+//     return forbidden ? { forbiddenName: { value: control.value } } : null;
+//   };
+// }
+
+export const forbiddenNameValidator = (
+  control: AbstractControl,
+): ValidationErrors | null => {
+  return ['foo'].includes(control.value)
+    ? { forbiddenName: 'Name is not allowed' }
+    : null;
+};
+
+export const asyncRoleValidator = (
+  control: AbstractControl,
+): Observable<ValidationErrors | null> => {
+  const allowedRoles = ['admin', 'dev'];
+  return of(control.value).pipe(
+    map((value) => {
+      return allowedRoles.includes(value)
+        ? null
+        : { forbiddenRole: 'Role is not allowed' };
+    }),
+  );
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PaginationComponent],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  currentPage = 1;
-  changePage(page: number): void {
-    this.currentPage = page;
-    console.log('changed current page to ', page);
+  fb = inject(NonNullableFormBuilder);
+  form = this.fb.group({
+    firstname: this.fb.control('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(5),
+        // forbiddenNameValidator(['foo']),
+        forbiddenNameValidator,
+      ],
+    }),
+    role: this.fb.control('', {
+      asyncValidators: [asyncRoleValidator],
+      updateOn: 'blur',
+    }),
+  });
+
+  get firstname() {
+    return this.form.controls.firstname;
   }
 
-  itemsChange(items: string[]): void {
-    console.log('parent itemsChange', items);
+  onSubmit() {
+    console.log(this.form.getRawValue(), this.form.controls.role.errors);
   }
 }
