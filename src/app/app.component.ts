b57e5707a6a692b0f9c3709b531fdf30ef075362
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-05-05 21:15:59 +0200

    478

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component, Input, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  input,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalendarComponent } from './calendar/calendar.component';
+import { BehaviorSubject, delay, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalendarComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CommonModule],
+  template: ` <div>I'm {{ loading$ | async }} {{ response$ | async }}</div> `,
   styleUrl: './app.component.css',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  currentPage = input.required<number>();
+  // message: string = 'loading...';
+  loading$ = new BehaviorSubject(true);
+
+  response$ = of('response').pipe(
+    delay(0),
+    tap(() => this.loading$.next(false)),
+  );
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    // this.message$.next('all done loading');
+    // setTimeout(() => {
+    // this.message = 'all done loading';
+    // });
+    // this.cdr.detectChanges();
+  }
+
+  // ngAfterContentChecked() {
+  //   this.message = 'all done loading :)';
+  // }
 }
