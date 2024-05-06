9cc505048bd573ed571b970798a99b9a82c5bc23
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-05-06 21:26:03 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
   Component,
   Input,
   input,
+  inject
 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BehaviorSubject, delay, of, tap } from 'rxjs';
@@ -15,27 +16,13 @@ import { BehaviorSubject, delay, of, tap } from 'rxjs';
   imports: [RouterOutlet, CommonModule],
   template: ` <div>I'm {{ loading$ | async }} {{ response$ | async }}</div> `,
   styleUrl: './app.component.css',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  // message: string = 'loading...';
   loading$ = new BehaviorSubject(true);
 
   response$ = of('response').pipe(
     delay(0),
     tap(() => this.loading$.next(false)),
   );
-  constructor(private cdr: ChangeDetectorRef) {}
 
-  ngAfterViewInit() {
-    // this.message$.next('all done loading');
-    // setTimeout(() => {
-    // this.message = 'all done loading';
-    // });
-    // this.cdr.detectChanges();
-  }
-
-  // ngAfterContentChecked() {
-  //   this.message = 'all done loading :)';
-  // }
 }
