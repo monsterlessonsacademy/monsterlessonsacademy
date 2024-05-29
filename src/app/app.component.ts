9e3836e0a64f768eb9e500045a9cf34b701b2e2b
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-05-29 20:33:06 +0200

    testing-track-by

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child.component';
-import { PortalExampleComponent } from './portal-example.component';
-import { OverlayExampleComponent } from './overlay-example.component';
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    ChildComponent,
-    PortalExampleComponent,
-    OverlayExampleComponent,
-  ],
+  imports: [CommonModule, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  arr = new BehaviorSubject([
+    { id: '1', period: '01-05', date: '01-05' },
+    { id: '2', period: '02-05', date: '02-05' },
+    { id: '3', period: '03-05', date: '03-05' },
+  ]);
+
+  changeArr(): void {
+    this.arr.next([
+      { id: '1', period: '09-05', date: '09-05' },
+      { id: '2', period: '02-05', date: '02-05' },
+      { id: '3', period: '03-05', date: '03-05' },
+    ]);
+  }
+
+  identify(index: any, item: any) {
+    return item.period;
+  }
+
+  testRenderParent() {
+    console.log('testRender parent');
+    return true;
+  }
+}
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,39 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ModalComponent } from './modal/modal.component';
 import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'app-content',
-  standalone: true,
-  template: `Hello user {{ user }}`,
-})
-export class ContentComponent {
-  user = 'foo';
-}
 
 @Component({
   selector: 'app-child',
   standalone: true,
-  imports: [ModalComponent, CommonModule, ContentComponent],
+  imports: [CommonModule],
   template: `
     <div>
-      <h2>Child</h2>
-      <button (click)="openModal()">Open modal</button>
-      <app-modal *ngIf="isModalOpened" (closeModal)="closeModal()">
-        <ng-container modal-header>This is our header</ng-container>
-        <ng-container modal-body><app-content /></ng-container>
-      </app-modal>
+      <h2>Child {{ testRender() }}</h2>
     </div>
   `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChildComponent {
-  isModalOpened = false;
-  openModal(): void {
-    this.isModalOpened = true;
-  }
-
-  closeModal(): void {
-    this.isModalOpened = false;
+  testRender() {
+    console.log('testRender');
+    return true;
   }
 }
