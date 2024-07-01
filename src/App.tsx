3070710b10fb04ec07629486ed587214fdd5bb95
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-07-01 21:58:45 +0200

    492

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-// import { Button } from "@/components/ui/button";
 import StatefulLoginForm from "./components/StatefulLoginForm";
 
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <StatefulLoginForm />
-      {/* <Button variant="destructive">Click me</Button> */}
     </div>
   );
 };
