9e70642e8b5435161e15304cfc432410108ea849
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-07-06 14:03:14 +0200

    501

--- src/drizzle/0000_equal_ken_ellis.sql
+++
@@ -1,5 +0,0 @@
--- Current sql file was generated after introspecting the database
--- If you want to run this migration please uncomment this code before executing migrations
-/*
-
-*/
--- src/drizzle/0001_nice_silver_surfer.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"email" text NOT NULL,
-	"username" text NOT NULL,
-	"password" text NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
