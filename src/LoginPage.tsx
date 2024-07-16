362216c43cc78463e57df84c690b43cc5918f1ff
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-07-16 22:04:55 +0200

    506

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,32 +1,24 @@
-import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
+import {
+  Avatar,
+  Box,
+  Container,
+  FormControlLabel,
+  Paper,
+  TextField,
+  Typography,
+  Checkbox,
+  Button,
+  Grid,
+  Link,
+} from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
 import { Link as RouterLink } from "react-router-dom";
-import { FormEvent } from "react";
 
 const LoginPage = () => {
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log("handle submit");
-  };
+  const handleSubmit = () => console.log("login");
   return (
     <Container maxWidth="xs">
-      <Paper
-        elevation={10}
-        sx={{
-          marginTop: 8,
-          padding: 2,
-        }}
-      >
+      <Paper elevation={10} sx={{ marginTop: 8, padding: 2 }}>
         <Avatar
           sx={{
             mx: "auto",
@@ -37,13 +29,7 @@ const LoginPage = () => {
         >
           <LockOutlinedIcon />
         </Avatar>
-        <Typography
-          component="h1"
-          variant="h5"
-          sx={{
-            textAlign: "center",
-          }}
-        >
+        <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>
           Sign In
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
@@ -65,7 +51,7 @@ const LoginPage = () => {
             label="Remember me"
           />
           <Button type="submit" variant="contained" fullWidth sx={{ mt: 1 }}>
-            Sign in
+            Sign In
           </Button>
         </Box>
         <Grid container justifyContent="space-between" sx={{ mt: 1 }}>
