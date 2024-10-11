eff9f2df960e2fbc4b4e3fddec9a3dc45ce4004e
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-10-11 18:51:45 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,242 +1,57 @@
-import { useEffect, useRef, useState } from "react";
-import Big from "big.js";
+import React, { useState, useCallback } from "react";
 
-type State = {
-  total: string | null;
-  next: string | null;
-  operation: string | null;
+type UserType = {
+  id: string;
+  name: string;
 };
 
-const initialState: State = {
-  total: null,
-  next: null,
-  operation: null,
+type ListProps = {
+  users: UserType[];
+  onRemove: (userId: string) => void;
 };
 
-const isNumber = (item: string): boolean => {
-  return /[0-9]+/.test(item);
-};
+const initialUsers: UserType[] = [
+  {
+    id: "1",
+    name: "foo",
+  },
+  {
+    id: "2",
+    name: "bar",
+  },
+];
 
-const operate = (state: State): string => {
-  const one = Big(state.total ?? "0");
-  const two = Big(state.next ?? "0");
-
-  if (state.operation === "+") {
-    return one.plus(two).toString();
-  }
-
-  if (state.operation === "-") {
-    return one.minus(two).toString();
-  }
-
-  if (state.operation === "x") {
-    return one.times(two).toString();
-  }
-
-  if (state.operation === "÷") {
-    if (state.next === "0") {
-      alert("Divide by 0 error");
-      return "0";
-    } else {
-      return one.div(two).toString();
-    }
-  }
-
-  throw new Error(`Unknown operator ${state.operation}`);
-};
-
-const calculate = (state: State, name: string): State => {
-  if (name === "ac") {
-    return initialState;
-  }
-
-  if (isNumber(name)) {
-    if (state.next) {
-      const next = state.next === "0" ? name : state.next + name;
-      return { ...state, next };
-    }
-    return { ...state, next: name };
-  }
-
-  if (name === "=") {
-    if (state.next && state.operation) {
-      return {
-        total: operate(state),
-        next: null,
-        operation: null,
-      };
-    } else {
-      return state;
-    }
-  }
-
-  if (name === "%") {
-    if (!state.next) {
-      return state;
-    }
-
-    return {
-      ...state,
-      next: Big(state.next).div(Big("100")).toString(),
-    };
-  }
-
-  if (name === ".") {
-    if (!state.next) {
-      return { ...state, next: "0." };
-    }
-
-    if (state.next.includes(".")) {
-      return state;
-    }
-
-    return { ...state, next: state.next + "." };
-  }
-
-  if (name === "+/-") {
-    if (state.next) {
-      return { ...state, next: (-1 * parseFloat(state.next)).toString() };
-    }
-    if (state.total) {
-      return { ...state, total: (-1 * parseFloat(state.total)).toString() };
-    }
-  }
-
-  // User pressed an operation button and there is already an existing operation
-  if (state.operation) {
-    return {
-      total: operate(state),
-      next: null,
-      operation: name,
-    };
-  }
-
-  return {
-    total: state.next,
-    next: null,
-    operation: name,
-  };
+const List = ({ users, onRemove }: ListProps) => {
+  console.log("rendering list");
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.name} <span onClick={() => onRemove(user.id)}>X</span>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 const App = () => {
-  const [state, setState] = useState<State>(initialState);
-  const textRef = useRef<HTMLDivElement>(null);
-  const display = state.next || state.total || "0";
-  console.log("state", state);
-  const handleClick = (name: string): void => {
-    const newState = calculate(state, name);
-    setState(newState);
+  const [users, setUsers] = useState<UserType[]>(initialUsers);
+  const [text, setText] = useState<string>("");
+  const handleRemove = useCallback(
+    (userId: string) => {
+      console.log("handleRemove", userId);
+      const filteredUsers = users.filter((user) => user.id !== userId);
+      setUsers(filteredUsers);
+    },
+    [users]
+  );
+  const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
   };
-
-  useEffect(() => {
-    const adjustFontSize = () => {
-      const element = textRef.current!;
-      let fontSize = 70;
-
-      element.style.fontSize = `${fontSize}px`;
-
-      while (element.scrollWidth > element.clientWidth && fontSize > 1) {
-        fontSize--;
-        element.style.fontSize = `${fontSize}px`;
-      }
-    };
-
-    adjustFontSize();
-  }, [display]);
-
   return (
-    <div className="container">
-      <div className="calculator">
-        <div className="screen">
-          <div className="display" ref={textRef}>
-            {display}
-          </div>
-        </div>
-        <div className="button-panel">
-          <div>
-            <button onClick={() => handleClick("ac")} className="button">
-              AC
-            </button>
-            <button onClick={() => handleClick("+/-")} className="button">
-              +/-
-            </button>
-            <button onClick={() => handleClick("%")} className="button">
-              %
-            </button>
-            <button
-              onClick={() => handleClick("÷")}
-              className="button  operation"
-            >
-              ÷
-            </button>
-          </div>
-          <div>
-            <button onClick={() => handleClick("7")} className="button">
-              7
-            </button>
-            <button onClick={() => handleClick("8")} className="button">
-              8
-            </button>
-            <button onClick={() => handleClick("9")} className="button">
-              9
-            </button>
-            <button
-              onClick={() => handleClick("x")}
-              className="button  operation"
-            >
-              x
-            </button>
-          </div>
-          <div>
-            <button onClick={() => handleClick("4")} className="button">
-              4
-            </button>
-            <button onClick={() => handleClick("5")} className="button">
-              5
-            </button>
-            <button onClick={() => handleClick("6")} className="button">
-              6
-            </button>
-            <button
-              onClick={() => handleClick("")}
-              className="button  operation"
-            >
-              -
-            </button>
-          </div>
-          <div>
-            <button onClick={() => handleClick("1")} className="button">
-              1
-            </button>
-            <button onClick={() => handleClick("2")} className="button">
-              2
-            </button>
-            <button onClick={() => handleClick("3")} className="button">
-              3
-            </button>
-            <button
-              onClick={() => handleClick("+")}
-              className="button operation"
-            >
-              +
-            </button>
-          </div>
-          <div className="last-row">
-            <button onClick={() => handleClick("0")} className="button">
-              0
-            </button>
-            <button onClick={() => handleClick(".")} className="button">
-              .
-            </button>
-            <button
-              onClick={() => handleClick("=")}
-              className="button  operation"
-            >
-              =
-            </button>
-          </div>
-        </div>
-      </div>
+    <div>
+      <input type="text" value={text} onChange={handleText} />
+      <List users={users} onRemove={handleRemove} />
     </div>
   );
 };
