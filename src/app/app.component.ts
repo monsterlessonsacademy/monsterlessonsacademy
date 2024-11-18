f07de6e35e803278d76b23dfdde3ca4b64c90b48
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2024-11-18 12:07:42 +0100

    295-v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,43 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { CurrentUserService } from './currentUser.service';
+import {
+  AfterViewInit,
+  Component,
+  Directive,
+  ElementRef,
+  inject,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Directive({
+  selector: '[highlight]',
+  standalone: true,
+})
+export class HighlightDirective implements AfterViewInit {
+  elementRef = inject(ElementRef);
+
+  ngAfterViewInit() {
+    console.log(this.elementRef);
+    this.elementRef.nativeElement.style.background = 'yellow';
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, FormsModule, HighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  currentUserService = inject(CurrentUserService);
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.currentUserService.setCurrentUser();
-    }, 2000);
+export class AppComponent {
+  isActive = true;
+  name = 'foo';
+  changeName(): void {
+    this.name = 'bar';
   }
+  users = [
+    { id: 1, title: 'foo' },
+    { id: 2, title: 'bar' },
+    { id: 3, title: 'baz' },
+  ];
+  role = 'admin';
 }
---
+++ src/app/child-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  template: '<div>It is a child component</div>',
+})
+export class ChildComponent {}
