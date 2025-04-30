376b17485b22b084f25ee3a03310c47c838e878d
Oleksandr Kocherhin <monsterlessonsacademy@gmail.com>
2025-04-30 07:03:00 +0200

    WIP

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,9 +2,5 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  return (
-    <button type="text" onClick={() => setCount(count + 1)}>
-      Count: {count}
-    </button>
-  );
+  return <button onClick={() => setCount(count + 1)}>Count: {count}</button>;
 }
